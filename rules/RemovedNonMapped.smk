3e142ecc474469a2bcea508a8d3cd758d00737b8
Guillaume Le berre <leberreguillaume.glb@gmail.com>
2023-10-31 16:22:05 +0100

    Corrected the shell command in the rule to run with python script

--- rules/RemovedNonMapped.smk
+++ rules/RemovedNonMapped.smk
@@ -26,18 +26,17 @@ rule SamtoolsView:
         "../envs/samtools.yaml"
 
     params:
-        OutputMappings = expand(os.path.join(DATA_DIR_MG, "{project}/07_BWA_Mapped_Sequences/{sample}/"),
+        InputMappings = expand(os.path.join(DATA_DIR_MG, "{project}/07_BWA_Mapped_Sequences/{sample}/"),
             project = config["metagenomics"]["project"], sample = config["metagenomics"]["sample"]
             ),
         OutputFiltered = expand(os.path.join(DATA_DIR_MG, "{project}/08_Sam_Filtered/{sample}/"),
             project = config["metagenomics"]["project"], sample = config["metagenomics"]["sample"]
-            )
+            ),
+        Sample = config["metagenomics"]["sample"]
+    
     shell:
         """
-        for sam in {params.OutputMappings}; do 
-            samfile=${sam##*/}   
-            samtools view -S -F 2308 $sam -o {params.OutputFiltered}/{samfile%%.*}_Filtered.sam
-        done
+        python3 scripts/RunSamtools.py -mf {params.InputMappings} -of {params.OutputFiltered}
         touch {output}
         """
         # Firstly ${sam##*/} will remove the file name from the path. 
---
+++ scripts/RunSamtools.py
@@ -0,0 +1,40 @@
+import os, subprocess, argparse
+
+
+parser = argparse.ArgumentParser(description='Generating Genbank files from FASTA & GFF file format')
+parser.add_argument('-mf', '--MappingFolder', type = str, required = True, 
+                    help ='Give the directory with the mapping file present.')
+parser.add_argument('-of', '--OutputFilteredSam', type = str, required = True, 
+                    help ='Give the name of the outputfolder for the filtered SAM files.')
+args = parser.parse_args()
+
+
+# If the folder to store the Mapped Indexes is present will skip
+if os.path.exists(args.OutputFilteredSam):
+    pass
+# If the folder is not present it will >>
+else:
+    # Split up the full path of the folder
+    splitted_path = args.OutputFilteredSam.split('/')
+    # Cleaning the list containg empty indexes
+    splitted_path_cleaned = [d for d in splitted_path if d != '']
+    # Start from the root of the path
+    path_to_check = '/'    
+    # Loop over all the directories
+    for directory in splitted_path_cleaned:
+        # Iterate over the paths
+        path_to_check += f"{str(directory)}/"
+        # If it exists do nothing
+        if os.path.exists(path_to_check):
+            continue
+        # If not create the folder
+        else: 
+            os.mkdir(path_to_check)
+
+# Iterate over the SAM files to remove the unmapped reads from the alignment
+for file in os.listdir(args.MappingFolder):
+    #COmbining the path + file togheter
+    sam_file_path = os.path.join(args.MappingFolder, file)
+    basename = os.path.splitext(file)
+    # Performing the samtools command
+    subprocess.run(f"samtools view -@ 8 -S -F 2308 {sam_file_path} -o {args.OutputFilteredSam}/{basename[0]}_Filtered.sam", shell = True)
